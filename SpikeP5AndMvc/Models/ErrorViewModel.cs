da2b226b0cc8a1ed817b30af5b99f5e41f0809dc
Michel <elschepo@gmail.com>
2020-01-18 10:55:24 +0100

    Add project files.

---
+++ SpikeP5AndMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpikeP5AndMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
